83ce77d5357865c7cf202dda71802043a2788945
Marc Beaupre <jack.quack@gmail.com>
2013-12-03 14:36:21 -0500

    Fix: Multiple addition of the same except symbol.

--- lib/active_model/serializer/associations.rb
+++ lib/active_model/serializer/associations.rb
@@ -93,14 +93,18 @@ module ActiveModel
           if target_serializer
             target_serializer.new(object, source_serializer.options)
           elsif object.respond_to?(:active_model_serializer) && (ams = object.active_model_serializer)
-            _options = source_serializer.options
-            _options[:except] = Array(_options[:except])
             source_association = source_serializer.object.class.to_s.underscore.downcase
             plural_source_association = source_association+'s'
             except = ams._associations.keys.map(&:to_s).find do |association|
               association == source_association || association == plural_source_association
             end
-            _options[:except].push(except.to_sym) if not except.nil?
+            _options = source_serializer.options
+            _options[:except] = Array(_options[:except])
+            if not except.nil?
+              if not _options[:except].include? except.to_sym
+                _options[:except].push(except.to_sym)
+              end
+            end
             ams.new(object, _options)
           else
             object
